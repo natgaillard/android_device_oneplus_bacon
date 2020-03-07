e3cf0e79e5066f9d05bd8859f132fcf405c0ef28
natgaillard <61752388+natgaillard@users.noreply.github.com>
2020-03-07 17:21:45 +0100

    Update and rename arrow_bacon.mk to havoc_bacon.mk

--- havoc_bacon.mk
+++ havoc_bacon.mk
@@ -21,14 +21,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/bacon/bacon.mk)
 
 # Inherit some common Havoc stuff.
-$(call inherit-product, vendor/arrow/config/common_full_phone.mk)
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
 # Device identifications
-PRODUCT_NAME := arrow_bacon
+PRODUCT_NAME := havoc_bacon
 PRODUCT_DEVICE := bacon
 PRODUCT_BRAND := oneplus
 PRODUCT_MANUFACTURER := OnePlus
